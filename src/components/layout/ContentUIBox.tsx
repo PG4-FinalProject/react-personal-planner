5903733bdd0d32ab5d44a7c87426aa70b605c34f
youjeans-s <molebest22@gmail.com>
2025-01-22 14:40:47 +0900

    fix: ContentUIBox 여백과 간격을 유연하게 설정

--- src/components/layout/ContentUIBox.tsx
+++ src/components/layout/ContentUIBox.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledBox = styled.div<{ bgColor?: string }>`
-  padding: 16px;
+const StyledBox = styled.div<{
+  bgColor?: string;
+  padding?: string;
+  margin?: string; // margin을 선택적으로 수정
+}>`
+  padding: ${props => props.padding || '16px'}; // 패딩을 유연하게 설정
   border-radius: 4px;
-  background-color: ${props => props.bgColor || '#fff'}; //박스 색 유연하게 설정
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-  margin: 16px;
+  background-color: ${props => props.bgColor || '#fff'};
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  margin: ${props => props.margin || '16px'}; // 마진을 유연하게게 설정
 `;
 
 interface ContentUIBoxProps {
   children: React.ReactNode;
   bgColor?: string;
+  padding?: string;
+  margin?: string;
 }
 
-const ContentUIBox: React.FC<ContentUIBoxProps> = ({ children, bgColor }) => {
-  return <StyledBox bgColor={bgColor}>{children}</StyledBox>;
+const ContentUIBox: React.FC<ContentUIBoxProps> = ({
+  children,
+  bgColor,
+  padding,
+  margin,
+}) => {
+  return (
+    <StyledBox bgColor={bgColor} padding={padding} margin={margin}>
+      {children}
+    </StyledBox>
+  );
 };
 
 export default ContentUIBox;
