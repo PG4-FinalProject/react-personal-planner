e6526d9815e96f4dfc2b4a4233d757f755123b69
yschoi <yschoi9733@gmail.com>
2025-02-01 14:28:16 +0900

    refactor: MainLayout css 수정

--- src/components/layout/Content.tsx
+++ src/components/layout/Content.tsx
@@ -6,19 +6,9 @@ import { palette } from '../../styles/palette';
 const ContentWrapper = styled.div<{
   color?: string;
 }>`
-  flex: 1;
-  max-width: 534px;
   min-height: calc(100vh - 130px); // 헤더와 푸터의 높이를 고려한 최소 높이
-  margin: 0 auto;
-  margin-bottom: 65px;
-  margin-top: 65px;
   overflow-y: auto;
   background-color: ${props => props.color || palette.background};
-`;
-
-const ContentInner = styled.div`
-  width: 100%;
-  height: 100%;
 `;
 
 interface ContentProps {
@@ -27,11 +17,7 @@ interface ContentProps {
 }
 
 const Content: React.FC<ContentProps> = ({ children, color }) => {
-  return (
-    <ContentWrapper color={color}>
-      <ContentInner>{children}</ContentInner>
-    </ContentWrapper>
-  );
+  return <ContentWrapper color={color}>{children}</ContentWrapper>;
 };
 
 export default Content;
