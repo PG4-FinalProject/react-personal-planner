60391dc3e6d9e83e58c77b97a6df886e1de49009
yschoi <yschoi9733@gmail.com>
2025-02-03 14:21:36 +0900

    chore: api 호출 url 수정

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -2,9 +2,9 @@ import { JoinReqBody, LoginReqBody } from '../types/user.type';
 import { requestHandler } from './http';
 
 export const login = async (loginData: LoginReqBody) => {
-  return await requestHandler('post', 'users/login', loginData);
+  return await requestHandler('post', '/users/login', loginData);
 };
 
 export const join = async (joinData: JoinReqBody) => {
-  return await requestHandler('post', 'users/join', joinData);
+  return await requestHandler('post', '/users/join', joinData);
 };
