b733544ce409c26ce64574514d12415c45dad5e7
yschoi <yschoi9733@gmail.com>
2025-02-03 14:20:53 +0900

    refactor: reset.css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/Routing'; // routes 경로
-import './index.css'; // 필요한 경우 CSS 파일을 추가
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './apis/queryClient';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <AppRoutes /> {/* 라우트 컴포넌트 사용 */}
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AppRoutes /> {/* 라우트 컴포넌트 사용 */}
+      </Router>
+    </QueryClientProvider>
   );
 };
 
