6a99b8dea5b1457b192f5013551c7cdd310a3aac
yschoi <yschoi9733@gmail.com>
2025-02-05 20:37:15 +0900

    feat: api 호출 에러 상황 처리

--- src/hooks/useCategory.ts
+++ src/hooks/useCategory.ts
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react';
 import { CategoryI } from '../types/category.type';
 import { getCategories } from '../apis/category.api';
+import { useNavigate } from 'react-router-dom';
 
 export const useCategory = () => {
   const [categories, setCategories] = useState<CategoryI[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    getCategories().then(res => {
-      setCategories(res);
-    });
+    getCategories().then(
+      res => {
+        setCategories(res);
+      },
+      err => {
+        navigate(-1);
+      },
+    );
   }, []);
 
   return {
