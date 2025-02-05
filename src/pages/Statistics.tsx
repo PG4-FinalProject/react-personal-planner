424149c1d360a8b8eb6329c701cb5e81ef249e58
yschoi <yschoi9733@gmail.com>
2025-02-05 17:46:11 +0900

    feat: statistics 페이지 구현

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -2,14 +2,45 @@ import React from 'react';
 import MainLayout from '../components/layout/MainLayout'; // MainLayout 경로를 맞춰주세요.
 import ContentUIBox from '../components/layout/ContentUIBox';
 import { palette } from '../styles/palette';
+import Title from '../components/common/Title';
+import styled from 'styled-components';
+
+const StatCountBox = styled.div`
+  margin: 14px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StatCountName = styled.span`
+  color: ${palette.gray};
+`;
+
+const StatCount = styled.span`
+  font-weight: bold;
+`;
+
+const ProgressBar = styled.div`
+  width: 100%;
+  height: 8px;
+  background-color: ${palette.blue};
+`;
 
 const Statistics: React.FC = () => {
+  const TodayProgressPercent = 60;
+
   return (
     <MainLayout>
       <ContentUIBox bgColor={palette.white}>
-        <h1>통계 페이지</h1>
-        <p>여기에 통계 관련 콘텐츠를 추가하세요.</p>
-        {/* 통계 컴포넌트나 기능을 여기에 추가할 수 있습니다. */}
+        <Title>일간 통계</Title>
+        <StatCountBox>
+          <StatCountName>완료된 일정</StatCountName>
+          <StatCount>3건</StatCount>
+        </StatCountBox>
+        <StatCountBox>
+          <StatCountName>진행중 일정</StatCountName>
+          <StatCount>2건</StatCount>
+        </StatCountBox>
+        <ProgressBar></ProgressBar>
       </ContentUIBox>
     </MainLayout>
   );
